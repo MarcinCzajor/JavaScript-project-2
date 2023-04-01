c31a29bbcf00f3bb20710a9e38dd6d920db1d35a
Marcin <120125611+MarcinCzajor@users.noreply.github.com>
2023-04-01 13:09:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+const currencyAPI = "https://api.nbp.pl/api/exchangerates/tables/a/";
+
+const inputCurrency = document.querySelector("#cur-amount");
+const currButton = document.querySelector("#exchange");
+const curSelect = document.querySelector("#cur-select");
+const exchValue = document.querySelector("#exch-value");
+
+const convertCurrency = async () => {
+	const inputValue = inputCurrency.value;
+	const selectedValue = curSelect.value;
+	fetch(currencyAPI)
+		.then((response) => response.json())
+		.then((data) => {
+			const currecyTable = data[0].rates;
+			const selectedCurrency = currecyTable.find(
+				(item) => item.code === selectedValue
+			);
+			exchValue.innerText = (
+				inputValue * selectedCurrency.mid
+			).toFixed(2);
+		})
+		.catch((err) => {
+			alert(err.message);
+		});
+};
+const validateInputValue = async () => {
+	if (inputCurrency.value < 1) {
+		alert("wpisz prawidłową wartość");
+
+	} else {
+		convertCurrency();
+		
+	}
+	inputCurrency.value = 0;
+	exchValue.innerText = "";
+	
+};
+
+currButton.addEventListener("click", validateInputValue);
+
+const exchangeAmount = document.createElement("span");
